24e55bed7ac1266729f1292c9ccd5d9efe1eac7c
Kodjo Dokouvi <33075451+FendShooter@users.noreply.github.com>
2019-11-17 12:39:04 -0500

    Update dbconnect.js

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-
-// const connectionURL = `mongodb://127.0.0.1:27017/employee-db`;
-const connectionURL = `mongodb+srv://pat123:7Smvi850zst35HYx@cluster0-wprtj.mongodb.net/employeesdb?retryWrites=true&w=majority`
+ const connectionURL = `mongodb://127.0.0.1:27017/employee-db`;
 mongoose.connect(connectionURL, {
   useCreateIndex: true,
   useNewUrlParser: true
